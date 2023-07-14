0ec7a708a58a2445f497b79d22f2ae3f0a7b3ea4
Kamogelo Mashike <marykamogelo387@gmail.com>
2023-07-14 08:38:22 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all single digit numbers of base 1
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	printf("%d", x);
+	printf('\n');
+	return (0);
+}
