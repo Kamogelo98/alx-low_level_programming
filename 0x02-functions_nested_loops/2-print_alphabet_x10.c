ecdb9b654c8f8fc9a0326f46a14329f7f95428a7
Kamogelo Mashike <marykamogelo387@gmail.com>
2023-07-18 12:41:56 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,17 +7,16 @@
 
 void print_alphabet_x10(void)
 {
-	char c;
-	int i = 0;
+	int i = 1;
 
-	while (i <= 9)
+	while (i <= 10)
 	{
-		for (c = 'a'; c <= 'z'; c++)
+		for (char c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
-			_putchar('\n');
-			i++;
 		}
+		_putchar('\n');
+		i++;
 	}
 }
 
