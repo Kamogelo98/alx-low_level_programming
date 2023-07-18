d822757e22619a48ac39b2e7b8eee469785529fe
Kamogelo Mashike <marykamogelo387@gmail.com>
2023-07-18 16:02:23 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * jack_bauer -  function that prints every minute of the day of Jack Bauer
+ * Return : Return: Always 0 (Success)
+ */
+
+void jack_bauer(void)
+{
+int i, j, k, h;
+
+for (i = 0; i <= 2; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+for (k = 0; k <= 5; k++)
+{
+for (h = 0; h <= 9; h++)
+{
+if (i >= 2 && j >= 4)
+{
+return;
+}
+_putchar(i + '0');
+_putchar(j + '0');
+_putchar(':');
+_putchar(k + '0');
+_putchar(h + '0');
+_putchar('\n');
+}
+}
+}
+}
+}
