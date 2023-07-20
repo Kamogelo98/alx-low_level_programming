29ddd09aa1a350cb45005f098319449a66701a17
Kamogelo Mashike <marykamogelo387@gmail.com>
2023-07-20 03:44:04 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit 0 - 9
+ * Return: 1 if digit else 0
+ */
+
+int _isdigit(int c)
+{
+        if ((c >= 48) && (c <= 57))
+        {
+                return (1);
+        }
+        return (0);
+}
