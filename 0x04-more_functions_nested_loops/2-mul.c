4512945df34d91af6480526f4013a679e1526349
Kamogelo Mashike <marykamogelo387@gmail.com>
2023-07-20 04:10:12 -0700

    ./2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a,b: values used
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	int product = a * b;
+
+	return (product);
+}
