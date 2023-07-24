31d7dde2767171a2c890e137e8b85836ef0c6644
Kamogelo Mashike <marykamogelo387@gmail.com>
2023-07-24 11:31:38 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,21 @@
+#include "main.h"
 
+/**
+ * puts2 - prints every other character of a string
+ * @str: this is a character to be checked.
+ */
+
+void puts2(char *str)
+{
+	int x, y = 0;
+
+	while (str[y] != '\0')
+	{
+		y++;
+	}
+	for (x = 0; x < y; x += 2)
+	{
+		_putchar (str[x]);
+	}
+	_putchar ('\n');
+}
