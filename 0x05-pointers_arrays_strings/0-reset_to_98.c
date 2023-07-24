af8b3a15d02ad6dfddc339bfcb5070075c5136ab
Kamogelo Mashike <marykamogelo387@gmail.com>
2023-07-24 05:23:43 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates value of n
+ * @n: integer ponter to a variable to be updated
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
