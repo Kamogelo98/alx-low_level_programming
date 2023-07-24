4aa23b4bd734bb5cdfd09fa97bddd1d9003fa39d
Kamogelo Mashike <marykamogelo387@gmail.com>
2023-07-24 08:18:15 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - reverse string, followed by a new line,
+ *
+ * @s: the parameter
+ *
+ * Return: on success,
+ *         on error, -1 is returned, and errno is set appropriately
+ */
+
+void rev_string(char *s)
+{
+	int i;
+
+	for (i = 0; i < (int)(strlen / 2); i++)
+	{
+		char temp;
+
+		temp = s[i];
+		s[i] = s[strlen - 1 - i];
+		s[strlen - 1 - i] = temp;
+	}
+}
