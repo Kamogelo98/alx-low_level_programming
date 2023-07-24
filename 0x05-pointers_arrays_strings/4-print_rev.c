57d2af020f3ed41a8447d03138945775fe41f0d3
Kamogelo Mashike <marykamogelo387@gmail.com>
2023-07-24 07:58:57 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_rev - prints a string in reverse, followed by a new line,
+ *
+ * @s: the parameter
+ *
+ * Return: on success, the number of characters written
+ *         on error, -1 is returned, and errno is set appropriately
+ */
+
+void print_rev(char *s)
+{
+	int i = strlen(s) - 1;
+
+	for (;i >= 0; i--)
+	{
+
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+
+}
