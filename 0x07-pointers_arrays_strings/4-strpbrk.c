3a8ed95958087b54dbd7a136d851846ff9ba0978
Kamogelo Mashike <marykamogelo387@gmail.com>
2023-08-01 03:43:35 -0700

    ./4-strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: pointer
+ * @accept: pointer
+ * Return: pointer
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		char *temp = accept;
+
+		while (*temp != '\0')
+		{
+			if (*s == *temp)
+			{
+				return (s);
+			}
+			temp++;
+		}
+		s++;
+	}
+	return (NULL);
+}
