9a9833ffd0480b4b1c9a8d7e63587ba66f0da4fc
Kamogelo Mashike <marykamogelo387@gmail.com>
2023-08-02 06:32:05 -0700

    ./5-sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -17,6 +17,15 @@ int _sqrt_recursion(int n)
 	return (sqrt_recursion(n, 0));
 }
 
+/**
+ * sqrt_recursion - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ */
+
 int sqrt_recursion(int n, int i)
 {
 	if (i * i == n)
