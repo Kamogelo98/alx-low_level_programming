8023adad07c8f86f2e87ee72a3a97a5d1bc46ec3
Kamogelo Mashike <marykamogelo387@gmail.com>
2023-08-02 07:28:59 -0700

    ./6-prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <math.h>
 /**
  * is_prime_number - checks if a given int is a prime num
  * @n: given int
@@ -9,14 +9,29 @@
 
 int is_prime_number(int n)
 {
-	int i;
-
-	if (n < 2)
+	if (n <= 1)
+	{
 		return (0);
+	}
 
-	for (i = 2; i * i <= n; i++)
-		if (n % i == 0)
+	if (n <= 3)
+	{
+		return (1);
+	}
+
+	if (n % 2 == 0 || n % 3 == 0)
+	{
+		return (0);
+	}
+
+	int i = 5;
+	while (i * i <= n)
+	{
+		if (n % i == 0 || n % (i + 2) == 0)
+		{
 			return (0);
-
+		}
+		i += 6;
+	}
 	return (1);
 }
